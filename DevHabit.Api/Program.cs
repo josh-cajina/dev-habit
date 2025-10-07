12f44c6c13fab266eef3f613becad6b51da0606f
Joshua Cajina <josh.cajina@gmail.com>
2025-10-07 16:32:14 -0600

    Finish exception handling lesson

--- DevHabit.Api/Program.cs
+++ DevHabit.Api/Program.cs
@@ -1,5 +1,6 @@
 using DevHabit.Api.Database;
 using DevHabit.Api.Extensions;
+using DevHabit.Api.Middleware;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
@@ -20,13 +21,16 @@ builder.Services.AddControllers(options =>
 
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
-builder.Services.AddProblemDetails(options => 
+builder.Services.AddProblemDetails(options =>
 {
-    options.CustomizeProblemDetails = context => 
+    options.CustomizeProblemDetails = context =>
     {
         context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
     };
 });
+
+builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 builder.Services.AddOpenApi();
 
@@ -66,6 +70,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
 
 app.MapControllers();
 
