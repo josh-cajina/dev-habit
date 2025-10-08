ad43c5415b3bb5791c28afa4cf8eb11e49f6501a
Joshua Cajina <josh.cajina@gmail.com>
2025-10-08 00:56:33 -0600

    Finish implementing sorting and pagination lessons

--- DevHabit.Api/DTOs/Habits/HabitDto.cs
+++ DevHabit.Api/DTOs/Habits/HabitDto.cs
@@ -2,11 +2,6 @@
 
 namespace DevHabit.Api.DTOs.Habits;
 
-public sealed record HabitsCollectionDto 
-{
-    public List<HabitDto> Data { get; init; }
-}
-
 public record HabitDto
 {
     public required string Id { get; init; }
---
+++ DevHabit.Api/Services/Sorting/SortMappingProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq.Dynamic.Core;
+
+namespace DevHabit.Api.Services.Sorting;
+
+public sealed class SortMappingProvider(IEnumerable<ISortMappingDefinition> sortMappingDefinitions)
+{
+    public SortMapping[] GetMappings<TSource, TDestination>()
+    {
+        SortMappingDefinition<TSource, TDestination>? sortMappingDefinition = sortMappingDefinitions
+            .OfType<SortMappingDefinition<TSource, TDestination>>()
+            .FirstOrDefault();
+
+        if (sortMappingDefinition is null)
+        {
+            throw new InvalidOperationException($"The mapping from '{typeof(TSource).Name}' into '{typeof(TDestination).Name}' isn't defined");
+        }
+
+        return sortMappingDefinition.Mappings;
+    }
+
+    public bool ValidateMappings<TSource, TDestination>(string? sort) 
+    {
+        if (string.IsNullOrWhiteSpace(sort))
+        {
+            return true;
+        }
+
+        var sortFields = sort
+            .Split(',')
+            .Select(sortField => sortField.Trim().Split(' ')[0])
+            .Where(sortField => !string.IsNullOrWhiteSpace(sortField))
+            .ToList();
+
+        SortMapping[] mapping = GetMappings<TSource, TDestination>();
+
+        return sortFields.All(sortField => mapping.Any(m => m.SortField.Equals(sortField, StringComparison.OrdinalIgnoreCase)));
+    }
+}
